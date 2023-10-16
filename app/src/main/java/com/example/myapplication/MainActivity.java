d48089a58361c0d3b4c0290a97e03b809aff2130
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-10-16 11:56:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    private int STROGAGE_PERMISSION_CODE = 23;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void next(View view){
+        Intent intent = new Intent(MainActivity.this. MainActivity2.class);
+    }
+
+    public void savePublic(View view){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        String info = editText.getText().toString();
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File myFile = new File(folder, "myData1.txt");
+        writeData(myFile, info);
+        editText.setText("");
+    }
+
+    public void savePrivate(View view){
+        String info = editText.getText().toString();
+        File folder = getExternalFilesDir("arvita");
+        File myFile = new File(folder, "myData2.txt");
+        writeData(myFile, info);
+        editText.setText("");
+    }
+    private void writeData(File myFile, String data){
+        FileOutputStream fileOutputStream = null;
+        try {
+            System.out.println("Tes");
+            Toast.makeText(this, "done" + myFile.getAbsolutePath(), Toast.LENGTH_SHORT.show());
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null){
+                try {
+                    fileOutputStream.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
