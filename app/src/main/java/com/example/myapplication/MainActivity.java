20bddcf0e701b9cec5493973bff2dc2677ab6798
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-10-17 12:08:13 +0700

    fix MainActivity2.main can't show

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,9 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText2);
     }
     public void next(View view){
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
     }
 
     public void savePublic(View view){
